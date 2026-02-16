74ae606d2b5dc68f228cb3f742825f9788d3c149
Pulkit Kishan Vijaywargiapulkitlab <261796676+pulkitlab@users.noreply.github.com>
2026-02-16 23:52:45 +0000

    Updated tip calculator

--- Python_learnings/Projects/tip_calculator.py
+++ Python_learnings/Projects/tip_calculator.py
@@ -1,5 +1,5 @@
 # This program is created to calculate the tip
-amount = float(input(print("Enter the amount")))
-tip = float(input(print("Enter the tip in percentage")))
+amount = float(input("Enter the amount: "))
+tip = float(input(print("Enter the tip in percentage: ")))
 tip_amount=float(amount*(tip/100))
 print("Your tip value is" + str(tip_amount))
